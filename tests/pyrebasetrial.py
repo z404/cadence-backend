cc65420dc31b55c67c6e878e8d8240144ec569be
z404 <anishr890@gmail.com>
2021-03-04 11:05:28 +0530

    refactor: changed code to be compatible with black linting

--- tests/pyrebasetrial.py
+++ tests/pyrebasetrial.py
@@ -1,4 +1,4 @@
-'''
+"""
 Standalone program to test the usage of pyrebase
 Pyrebase package helps python interact with Google firebase realtime database
 Features:
@@ -11,54 +11,56 @@ For this project, modification of realtime database and creation of listeners is
 As this is the backend code, authentication for this program to access the firebase database should not be nessesary
 This is why a service account should be generated for this program
 requirements: pyrebase4 (https://github.com/nhorvath/Pyrebase4)
-'''
-#Importing pyrebase
+"""
+# Importing pyrebase
 import pyrebase
 import time
-#Getting creds from the creds file
-with open('creds.txt') as file:
+
+# Getting creds from the creds file
+with open("creds.txt") as file:
     data = file.readlines()
-    apikey = data[2].strip('\n')
-    authDomain = data[3].strip('\n')
-    databaseurl = data[4].strip('\n')
-    storagebucket = data[5].strip('\n')
-#Creating the config dictionary using the credentials
+    apikey = data[2].strip("\n")
+    authDomain = data[3].strip("\n")
+    databaseurl = data[4].strip("\n")
+    storagebucket = data[5].strip("\n")
+# Creating the config dictionary using the credentials
 config = {
-  "apiKey": apikey,
-  "authDomain":authDomain,
-  "databaseURL": databaseurl,
-  "storageBucket":storagebucket,
-  "serviceAccount": "serviceacc.json"
+    "apiKey": apikey,
+    "authDomain": authDomain,
+    "databaseURL": databaseurl,
+    "storageBucket": storagebucket,
+    "serviceAccount": "serviceacc.json",
 }
 
-#Creating the firebase object
+# Creating the firebase object
 firebase = pyrebase.initialize_app(config)
 
-#Creating the database object (other objects include storage and authentication)
+# Creating the database object (other objects include storage and authentication)
 db = firebase.database()
 
-#Trial code to pull information from the database. Return type is a dictionary of data
-#.child canb be used to download only specific information from a certain path
+# Trial code to pull information from the database. Return type is a dictionary of data
+# .child canb be used to download only specific information from a certain path
 for i in db.get().each():
     print(i.val())
 
-#Trial Code to push information to the database
-#.child can be used to push to specific location
+# Trial Code to push information to the database
+# .child can be used to push to specific location
 data = {"trial": "push to firebase"}
 db.push(data)
-#Pushing using the push function to the database gives a unique key to the data, which might not be easy to retrieve
-#In such cases, update() can be used instead of push
+# Pushing using the push function to the database gives a unique key to the data, which might not be easy to retrieve
+# In such cases, update() can be used instead of push
 
-#Creating a listening stream on a specific path
-#Function call on listener event:
+# Creating a listening stream on a specific path
+# Function call on listener event:
 def stream_handler(message):
-    print(message["event"]) #Type of event that occured: get, put, modify, delete
-    print(message["path"]) #Path of the event that occured: ".", "/users" etc
-    print(message["data"]) #Data that was affected by the event: {"name":"Anish"}
+    print(message["event"])  # Type of event that occured: get, put, modify, delete
+    print(message["path"])  # Path of the event that occured: ".", "/users" etc
+    print(message["data"])  # Data that was affected by the event: {"name":"Anish"}
 
-#Creating the streaming object
+
+# Creating the streaming object
 my_stream = db.stream(stream_handler)
 time.sleep(45)
 
-#Closing the stream
+# Closing the stream
 my_stream.close()
